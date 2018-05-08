a23f03692da162efc52ef1a849790dfa25ca9856
Ahmadali <ahmadali.azizov@outlook.com>
2018-05-08 17:46:29 +0000

    automatically backed up by learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -18,12 +18,12 @@ def get_user_input
   input = gets.chomp
 end
 
-def end_game
-  
+def end_game(card_total)
+  puts "Sorry, you hit #{card_total}. Thanks for playing!"
 end
 
 def initial_round
-  # code #initial_round here
+  
 end
 
 def hit?
